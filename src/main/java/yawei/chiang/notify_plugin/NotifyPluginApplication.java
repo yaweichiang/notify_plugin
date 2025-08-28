ac7b12b54fc61f1e7cf6254323b14a56c1ffb566
yaweichiang <yawei19901212@gmail.com>
2025-08-28 11:00:56 +0800

    build project

---
+++ src/main/java/yawei/chiang/notify_plugin/NotifyPluginApplication.java
@@ -0,0 +1,13 @@
+package yawei.chiang.notify_plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyPluginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yawei/chiang/notify_plugin/NotifyPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package yawei.chiang.notify_plugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
